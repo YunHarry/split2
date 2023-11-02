08ead5fe1b74ebc64aa7673fd133222ed46df783
Harry <harry.backend@gmail.com>
2023-11-02 10:43:54 +0900

    protocol test updated

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
+    if request.method == "POST":
+        # POST일때 어떤 값을 처리할지 적어줌
+        temp = request.POST.get('email')
+        return render(request, "accountapp/hello_world.html",
+                      context={"temp": temp})
+
     temp = "techit"
+
     return render(request, "accountapp/hello_world.html",
                   context={"temp": temp})
